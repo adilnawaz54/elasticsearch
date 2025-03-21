eb562dc6a072438775879bbb906ef70e7a3ae865
adilnawaz54 <adilnawaz54@gmail.com>
2025-03-21 16:26:19 +0530

    Update backend.tf

--- elasticsearch-terraform/backend.tf
+++ elasticsearch-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "adil-tf-bucket"  
+    bucket = "adil-bucket-es"  
     key    = "elasticsearch/terraform.tfstate" 
     region = "us-east-1"                    
     encrypt = true                        
